fe9611c894fe1d35a1d75692239ce7ef5515af2e
holy0spirit <107628145+holy0spirit@users.noreply.github.com>
2022-06-30 18:30:15 +0100

    Organizing
    Organizing file according to folders

--- model/mHTMLclass.py
+++ model/mHTMLclass.py
@@ -13,3 +13,5 @@ class mHTMLclass:
     mOpenHeadings1 = '<h1>'
     mCloseHeadings1 = '</h1>'
     mLinkJS = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'
+    mContainer = '<div class="container">'
+    mCloseDiv = '</div>'
